c13730c083dd11dff15c468526356fb84b8826be
Sam Jett <samjett247@gmail.com>
2019-06-11 12:50:23 -0500

    Updating w/ haskell

---
+++ Haskell/ctci/baby.hs
@@ -0,0 +1,10 @@
+doubleUs x y = x * 2 + y * 2
+
+doubleMe x = x + x
+truly = False
+
+checkStrDuplicates str_in = True
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+checkGreater name = name > "Sam"
+doubleUs x,y = x**2+y**2:wq:wq
+
---
+++ Haskell/fzach.hs
@@ -0,0 +1,12 @@
+doubleMe x = x + x
+conan="Its a me conan the dickhead"
+doubleSmall x = if x > 100 
+                    then x
+                    else x^2
+a=69
+remi =["I am Hungry" ,  "Food"]
+mylist = [1,2,3,4,5]
+myNewList = take 3 (reverse mylist)
+
+
+
